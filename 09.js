5bee2359a96152bcc5893d2c4129b3bf40f49322
Lindapazw <lindacsndelapaz@gmail.com>
2023-01-19 11:22:48 -0300

    add añadir elemento al array

--- 09.js
+++ 09.js
@@ -4,6 +4,10 @@ const tecnologias = ["html", "css", "java"]
 
 // añadir elementos al array
 
+tecnologias.push("node") // añade al final del array 
+tecnologias.unshift("node.js") // añade al inicio del array 
+
+
 // eliminar elementos del array
 
 // reemplazar elementos del array
