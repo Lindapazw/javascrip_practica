06884fbfdc01885e7720c39386cb67bc7da15007
Lindapazw <lindacsndelapaz@gmail.com>
2023-01-19 11:29:06 -0300

    add eliminar elementos modificando el original

--- 09.js
+++ 09.js
@@ -7,11 +7,17 @@ const tecnologias = ["html", "css", "java"]
 // tecnologias.push("node") // añade al final del array 
 // tecnologias.unshift("node.js") // añade al inicio del array 
 
-const nuevoArreglo = [...tecnologias, "GraphQL"] // no modifica el arreglo original
+// const nuevoArreglo = [...tecnologias, "GraphQL"] // no modifica el arreglo original
 
 // eliminar elementos del array
+
+tecnologias.pop() // eliminar del final 
+tecnologias.shift() // eliminar del inicio
+tecnologias.splice(2) // elimina todo a partir del 2
+tecnologias.splice(2,1) // elimina 1 a partir del lugar 2 
+
+
 
 // reemplazar elementos del array
 
 console.table(tecnologias)
-console.table(nuevoArreglo)
