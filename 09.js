91715e433d2bace0eb91c27ce44ce7bf1e379d00
Lindapazw <lindacsndelapaz@gmail.com>
2023-01-19 11:22:20 -0300

    add operaciones en arrays

---
+++ 09.js
@@ -0,0 +1,11 @@
+// Operaciones en los arreglos 
+
+const tecnologias = ["html", "css", "java"]
+
+// añadir elementos al array
+
+// eliminar elementos del array
+
+// reemplazar elementos del array
+
+console.table(tecnologias)
