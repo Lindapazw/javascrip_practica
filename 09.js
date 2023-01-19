1d76edb4805cf372ddb51f139a0d132c9ab53c65
Lindapazw <lindacsndelapaz@gmail.com>
2023-01-19 11:45:46 -0300

    add traer todos los que sean iguales a

--- 09.js
+++ 09.js
@@ -2,22 +2,8 @@
 
 const tecnologias = ["html", "css", "java"]
 
-// añadir elementos al array
-
-// tecnologias.push("node") // añade al final del array 
-// tecnologias.unshift("node.js") // añade al inicio del array 
-
-// const nuevoArreglo = [...tecnologias, "GraphQL"] // no modifica el arreglo original
-
-// eliminar elementos del array
-
-// tecnologias.pop() // eliminar del final 
-// tecnologias.shift() // eliminar del inicio
-// tecnologias.splice(2) // elimina todo a partir del 2
-// tecnologias.splice(2,1) // elimina 1 a partir del lugar 2 
-
 const nuevoArreglo = tecnologias.filter( function(tech) {
-    return tech !== "html"
+    return tech === "html"
 })
 
 // reemplazar elementos del array
