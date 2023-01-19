e51237b60fad0f61aba63c1301c78f7ea17db2eb
Lindapazw <lindacsndelapaz@gmail.com>
2023-01-19 11:26:07 -0300

    add añadir elemento sin modificar el original

--- 09.js
+++ 09.js
@@ -4,12 +4,14 @@ const tecnologias = ["html", "css", "java"]
 
 // añadir elementos al array
 
-tecnologias.push("node") // añade al final del array 
-tecnologias.unshift("node.js") // añade al inicio del array 
+// tecnologias.push("node") // añade al final del array 
+// tecnologias.unshift("node.js") // añade al inicio del array 
 
+const nuevoArreglo = [...tecnologias, "GraphQL"] // no modifica el arreglo original
 
 // eliminar elementos del array
 
 // reemplazar elementos del array
 
 console.table(tecnologias)
+console.table(nuevoArreglo)
