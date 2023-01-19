7a233056617cde650b00438c2ed141deb2c46f2d
Lindapazw <lindacsndelapaz@gmail.com>
2023-01-18 21:17:38 -0300

    add variable let

--- 01.js
+++ 01.js
@@ -1,9 +1,14 @@
 // hola mundo 
 
-console.log('Hola mundo')
+console.log("Hola mundo")
 
 // variables - var - const - let 
+
+// se pueden reasignar un valor 
+// pueden inicial si un valor 
 
 let cliente = "Juan"
 console.log(cliente)
 
+let precioDescuento = 220 
+console.log(precioDescuento)
