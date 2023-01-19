6684f7e0e1aa4d47329dd05891d4d31fd88a4eb3
Lindapazw <lindacsndelapaz@gmail.com>
2023-01-19 09:52:35 -0300

    add string

--- 03.js
+++ 03.js
@@ -23,3 +23,11 @@ const numero3 = -100
 console.log(typeof numero1)
 console.log(typeof numero2)
 console.log(typeof numero3)
+
+// String o cadenas de tecto 
+
+const alumno = "Juan"
+let producto = "Monitos de 20 pulgadas"
+
+console.log(typeof alumno)
+console.log(typeof producto)
