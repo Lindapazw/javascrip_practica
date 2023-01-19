18cb3ee2bb10451fa7c5bb51fa311950a09a6b5d
Lindapazw <lindacsndelapaz@gmail.com>
2023-01-19 10:27:16 -0300

    add objeto

---
+++ 04.js
@@ -0,0 +1,18 @@
+// objeto 
+
+let myCar = {
+    make: 'Ford',
+    model: 'Mustang',
+    year: 1969
+};
+
+console.log(myCar)
+
+// mostrar como tabla
+
+console.table(myCar)
+
+// mostrar solo nombre
+
+console.log(myCar.model)
+
