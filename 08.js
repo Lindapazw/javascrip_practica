32b31c8bc0260052cd9ed8397d224bab92e0f9f1
Lindapazw <lindacsndelapaz@gmail.com>
2023-01-19 11:15:56 -0300

    add array

---
+++ 08.js
@@ -0,0 +1,13 @@
+// arrays o arreglos 
+
+const tecnologias = [20,30, "linda"]
+
+console.log(typeof tecnologias)
+console.log (tecnologias)
+console.table (tecnologias)
+
+// contar cantidad de elementos dentro del array
+console.log(tecnologias.length)
+
+// como string
+console.log(tecnologias.toString())
