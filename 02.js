fce771f7fa3a70d0fef786c2e3497c9bcf7b3b34
Lindapazw <lindacsndelapaz@gmail.com>
2023-01-19 09:35:44 -0300

    add const

---
+++ 02.js
@@ -0,0 +1,10 @@
+// const 
+
+const cliente = "Juan"
+console.log(cliente)
+
+const precioDescuento = 220 
+console.log(precioDescuento)
+
+// no se puede reasignar 
+// no puede iniciar sin un valor
