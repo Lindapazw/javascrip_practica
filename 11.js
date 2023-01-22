76505db36350a64b74f94c13425726361f39c3b0
Linda Paz <lindacsndelapaz@gmail.com>
2023-01-22 11:30:44 -0300

    add iterador forEach

---
+++ 11.js
@@ -0,0 +1,7 @@
+// iteradores en js
+const tecnologias = ['html','css','node','react']
+
+// forEach - accerder a cada elemento del array 
+tecnologias.forEach(function(tech){
+    console.log(tech)
+})
